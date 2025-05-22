a0f4c61a1dcd5efe68b9ea5d82da1a47a32aa79a
Donk3rn <itsxhenis@gmail.com>
2025-05-23 00:13:11 +0200

    Update felling.py

--- cogs/felling.py
+++ cogs/felling.py
@@ -9,10 +9,11 @@ class Felling(commands.Cog):
     async def felling(self, ctx):
         embed1 = discord.Embed(
             title="Felling – Fremgangsmåte",
-            description="Vi oppretter en ticket der syv tilfeldige fra teamet blir valgt ut. Disse må være moderator, Senior moderator, administrator og eier.\n"
-            "- De utvalgte ser så på søknaden og tar en avgjørelse.",
-            color=discord.Color.from_str("#00B7B3")
-        )
+            description="Vi oppretter en ticket der syv tilfeldige fra teamet blir valgt ut. Disse må være moderator, Senior moderator, administrator og eier.\n\n"
+                "- De utvalgte ser så på søknaden og tar en avgjørelse.",
+             color=discord.Color.from_str("#00B7B3")
+)
+
 
         embed2 = discord.Embed(
             title="Regler ved godkjent felling",
