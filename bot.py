d8775a078b29e4826a7b9f351210fb205c66f612
Donk3rn <itsxhenis@gmail.com>
2025-05-22 22:11:16 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,9 @@
+import os
 import discord
 from discord.ext import commands
-import os
-import asyncio
 from dotenv import load_dotenv
 
-load_dotenv()  # Laster .env-fila
-token = os.getenv("DISCORD_TOKEN")
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.members = True
@@ -15,18 +13,13 @@ bot = commands.Bot(command_prefix=".", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Logget inn som {bot.user}")
+    print(f"Logget inn som {bot.user}!")
 
-async def load_cogs():
-    for filename in os.listdir("./cogs"):
-        if filename.endswith(".py"):
-            await bot.load_extension(f"cogs.{filename[:-3]}")
+# Last inn cogs
+initial_extensions = ["cogs.staff", "cogs.felling"]
 
-async def main():
-    await load_cogs()
-    await bot.start(token)
+for ext in initial_extensions:
+    bot.load_extension(ext)
 
-asyncio.run(main())
-
-token = os.getenv("DISCORD_TOKEN")
-bot.run(token)
+# Start botten
+bot.run(os.getenv("DISCORD_TOKEN"))
